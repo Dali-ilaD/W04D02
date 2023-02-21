c34d419e8ba8a7f3bdb3debca2ca75bedade1783
AA Student <appacademy@Mini-34.local>
2023-02-21 13:47:46 -0800

    first

---
+++ .chess.rb
@@ -0,0 +1,10 @@
+class Employee
+
+    def initialize(name, title, salray, boss)
+        @name = name 
+        @salary = salary    
+        @title = title
+        @boss = boss
+    end
+    
+end
